1a37dde9281079b67b045808c17d3100cfb3a265
admin <gkaeogus7@gmail.com>
2023-12-04 13:18:39 +0900

    231204

--- src/main/java/hello/servlet/web/frontcontroller/v1/ControllerV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/ControllerV1.java
@@ -1,5 +1,6 @@
 package hello.servlet.web.frontcontroller.v1;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.rowset.serial.SerialException;
@@ -7,5 +8,5 @@ import java.io.IOException;
 
 public interface ControllerV1 {
 
-    void process(HttpServletRequest request, HttpServletResponse response) throws SerialException, IOException;
+    void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
 }
